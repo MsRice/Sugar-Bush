7a37539aae335d78839eaa8a2386b10b0a8735b2
Rice <patricemaxwell@yahoo.com>
2025-05-01 12:44:05 -0400

    Updated video

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import banner_video from '../img/sugar_bush_banner.mp4'
+import banner_video from '../img/sugar_bush_banner_short.mp4'
 const Banner = () => {
 
       
     return (
         <section id='Banner'> 
             <div className='banner__background--wrapper'>
-                <video className='banner__background--video' src={banner_video} autoplay controls controlsList='nofullscreen nodownload' muted ></video> 
+                <video className='banner__background--video' src={banner_video} autoplay='true' controls controlsList='nofullscreen nodownload' muted ></video> 
             </div>
         </section>
     );
